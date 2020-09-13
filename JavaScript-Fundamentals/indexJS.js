be4fca85870f8ecab44168a936abfa2d7fb5d815
Juan Rosales <rjuan361@gmail.com>
2020-09-13 12:27:23 -0700

    Added a Second Page to show JS
    I have added a second web page to display some more JS features

--- JavaScript-Fundamentals/indexJS.js
+++ JavaScript-Fundamentals/indexJS.js
@@ -1,4 +1,6 @@
 
+//----------------        index.html JavaScript       -------------------------
+
 // Basic function structure.
 function firstFunction()         // message is an argument
 {
@@ -16,4 +18,29 @@ function firstFunction2(message)         // message is an argument
 function startFunc()
 {
     alert("We are live boys! " + new Date());
+}
+
+
+
+//-------------------      eventHandler.html JS        ------------------------
+
+function windowChange()
+{
+    console.log("The window changed size!");        // Using console to track events in website.
+}
+
+function inputCheck()
+{
+    alert("Submitted!");
+}
+
+function newHandler() 
+{
+    console.log("Modifying our event handlers.")
+}
+
+// ----------  CLEANING OUR CODE   -----------
+// Button3 event handler.
+document.getElementById("button3").onclick = function() {
+    newHandler();
 }
