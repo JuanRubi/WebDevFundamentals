525726d7656116e724095ad99bb6d4128cc74910
Juan Rosales <rjuan361@gmail.com>
2020-09-20 17:57:16 -0700

    Added 3 small JS projects
    Here are 3 simple JS projects to conclude the JavaScript fundamentals.

--- JavaScript-Fundamentals/smallProjects/miniProjects.js
+++ JavaScript-Fundamentals/smallProjects/miniProjects.js
@@ -60,3 +60,61 @@ document.getElementById("drawingPad").onclick = function() {
     lineStroke.lineCap = "round";
     lineStroke.stroke();
 }
+
+
+//-----------------       Simple Animation        -------------------
+var live = true;
+
+// Function to get a light color.
+function getLightColor()
+{
+    var possibleValues = '0123456789ABCDEF'.split('');  // Splits string into array.
+    var lightColor = '#';   // Setting up light color in hex system.
+
+    // Randomly selecting a value to complete hex color value.
+    for(var i=0; i < 6; i++)
+    {
+        lightColor += possibleValues[Math.floor(Math.random()*16)];
+    }
+
+    return lightColor;
+}
+
+// Function that runs the show.
+function theShow()
+{
+    var danceRoom = document.getElementById("room");
+    var floor = danceRoom.getContext("2d");
+
+    var varyPosition = Math.floor((Math.random()*349) + 1);     // 350 max due to canvas height.
+    var color = getLightColor();
+
+    floor.lineWidth = 10;
+    floor.strokeStyle = color;
+    floor.moveTo(10,varyPosition); // Vertical position is changing.
+    floor.lineTo(290, varyPosition);
+    floor.lineCap = "round";
+    floor.stroke();
+
+    document.getElementById("lightsColor").innerHTML = color;
+}
+
+// Starting and Ending the show using onclick.
+var animation;
+
+document.getElementById("startShow").onclick = function() {
+    if(live)
+    {
+        animation = setInterval(theShow, 100);      // Starts animation.
+        live = false;       // Prevents starting the show if it already started.
+    }
+    else
+    {
+        alert("The show is live already!");
+    }
+}
+
+document.getElementById("endShow").onclick = function() {
+    clearInterval(animation);       // Ends animation.
+    live = true;        // Allows the show to start if stopped.
+}
